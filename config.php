9eb751d1d0dd46923cdef5b9c67dd0ca27f444fc
Andri ❌treamui <74765836+classyid@users.noreply.github.com>
2025-05-22 21:46:21 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('GOOGLE_SHEET_API_URL', 'https://script.google.com/macros/s/ID-GOOGLE-APPS-SCRIPT/exec');
+define('API_KEY', 'APIKEY');
+define('SENDER', 'SENDER');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', '<user>');
+define('DB_PASS', '<password>');
+define('DB_NAME', '<database>');
+
+session_start();
